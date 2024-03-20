80b14896cb25d14913fe4f3a58b20e3b64e22b2c
Ber-Vazq <bvaq20@hotmail.com>
2024-03-20 18:31:11 -0500

    should work

--- process_synchronization.c
+++ process_synchronization.c
@@ -1,18 +1,25 @@
-// Without using semaphores
-// NO SYNCHRONIZATION
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
+
 int i;
+sem_t s1, s2;
+
+// Function to be executed by the first thread
 void *Thread1()
 {
+    sem_wait(&s1);
     printf("I am first thread\n");
     for (i = 10; i < 20; i++)
     {
         printf("%d\n", i);
     }
+    sem_post(&s1);
 }
+
+// Function to be executed by the second thread
 void *Thread2()
 {
     printf("I am second thread\n");
@@ -21,20 +28,32 @@ void *Thread2()
         printf("%d\n", i);
     }
 }
-void(*Thread3())
+
+// Function to be executed by the third thread
+void *Thread3()
 {
+    sem_wait(&s2);
     printf("I am third thread\n");
     for (i = 20; i < 31; i++)
     {
         printf("%d\n", i);
     }
+    //sem_post(&s1);
 }
 int main()
 {
+    sem_init(&s1, 0, 0);
+    sem_init(&s2, 0, 0);
+
     pthread_t tid, tid2, tid3;
     pthread_create(&tid, NULL, Thread1, (void *)&tid);
     pthread_create(&tid2, NULL, Thread2, (void *)&tid2);
     pthread_create(&tid3, NULL, Thread3, (void *)&tid3);
     pthread_exit(NULL);
+
+    sem_destroy(&s1);
+    sem_destroy(&s2);
+
+    pthread_exit (NULL);
     return 0;
 }
