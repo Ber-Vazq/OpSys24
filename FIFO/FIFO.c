ffa70010020bb1070f6ca53a50754503c10e0d4a
Ber-Vazq <bvaq20@hotmail.com>
2024-04-18 13:02:57 -0500

    github copilot ftw

---
+++ FIFO/FIFO.c
@@ -0,0 +1,57 @@
+#define MAXLEN 50
+typedef struct {
+    int pages_num;
+    int curr;
+    int pages[MAXLEN];
+    int faults;
+    int used[MAXLEN];
+} memory;
+memory* memory_init(int n) {
+    memory* m;
+    m = (memory*)malloc(sizeof(memory));
+    m->pages_num = n;
+    m->curr = 0;
+    m->faults = 0;
+    return m;
+}
+
+int memory_FIFO(int num){
+    memory* m = memory_init(num);
+    int input;
+    printf("Replacement Policy = FIFO\n");
+    printf("-------------------------------------\n");
+    printf("Page   Content of Frames\n");
+    printf("----   -----------------\n");
+    while(1){
+        scanf("%d", &input);
+        if(input == -1){
+            break;
+        }
+        add_process(m, input);
+    }
+    printf("-------------------------------------\n");
+    printf("Number of page faults = %d\n", m->faults);//i love github copilot, and how well it reads my mind
+    return 0;
+}
+
+int search_process(memory* m, int process){
+    int num = m->pages_num;
+    for(int i = 0; i < num; i++){
+        if(m->pages[i] == process){
+            return 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int num;
+    char algo[6];
+
+    scanf("%d", &num);
+    scanf("%s", algo);
+
+    if(strcmp(algo, "FIFO") == 0){
+        memory_FIFO(num);
+    }
+}
